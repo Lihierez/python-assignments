d568362eaedf4fa34c1fc52e947c4fc9eda5fab6
Lihi Erez <lihierez@Lihis-Laptop.local>
2024-11-18 11:41:13 +0200

    adding code to file

---
+++ day03/rectangle.py
@@ -0,0 +1,6 @@
+width= int(input("Enter Width:"))
+length= int(input("Enter height:"))
+area= width*length
+circumference= 2*(width+length)
+print('Rectangle area is: ', area)
+print('Rectangle circumference is:', circumference)
